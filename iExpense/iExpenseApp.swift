2c1f8a3ef91b8cb3225ef607420af237866ac03c
VeeBee <vikas@triamis.com>
2023-07-08 10:13:28 +0800

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Vikas Bhandari on 8/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
